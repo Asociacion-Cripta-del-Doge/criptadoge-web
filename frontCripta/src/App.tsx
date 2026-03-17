5ed7abdfe28541f296676d19286efd662b33d56a
Cristina <cris.glez512@gmail.com>
2026-03-17 10:49:56 +0000

    Mejoro alguno de los estilos de los botones

--- frontCripta/src/App.tsx
+++ frontCripta/src/App.tsx
@@ -14,6 +14,18 @@ function App() {
           Asociación sin ánimo de lucro dedicada al ocio alternativo para jóvenes.
           Juegos de mesa, rol, videojuegos y mucho más.
         </p>
+
+        <div className="hero-buttons">
+          <a className="btn-pink">Únete al Club</a>
+          <a className="btn-outline">Ver Eventos</a>
+        </div>
+
+      {/* #wip */}
+        <div className="hero-carousel">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
       </section>
     </>
   )
